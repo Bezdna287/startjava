e5ff45a8baaf3b837fc10033f0d1044a9607ac7d
Bezdna287 <58191023+Bezdna287@users.noreply.github.com>
2019-12-01 21:57:48 +0100

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main(String[] args){
+		byte b = 64;
+		short sh = 4750;
+		int i = 95800;
+		long l = 159623478;
+		float f = 1.56f;
+		double d = 12.698751;
+		char ch ='C';
+		boolean boo = false;
+		System.out.println(b);
+		System.out.println(sh);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(ch);
+		System.out.println(boo);
+	}
+}
