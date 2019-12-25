378d3a68a22e7ca49ec8ab056b3f570f191747f5
Tamara <bezdnatm@gmail.com>
2019-12-25 12:57:28 +0100

    GuessNumber

---
+++ GuessNumber.java
@@ -0,0 +1,16 @@
+
+public class GuessNumber {
+
+	public static void main(String[] args) {
+		int a = 19;
+		int b = 40;
+		
+		if (a>b) {
+			System.out.println("Your number is less than guessed number");	}
+		if (a<b) {
+			System.out.println("Your number is more than guessed number");	}
+		if (a==b) {
+			System.out.println("You guessed right!");	} 
+	}
+
+}
